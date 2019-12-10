7393904f988303e1690f43c33320a44c93a9cd5c
Rich Seiffert <richard.seiffert@icloud.com>
2019-12-10 00:05:16 -0700

    All header/banner stuff is grid/flex. Moving on!

--- src/components/layout-banner-compact.js
+++ src/components/layout-banner-compact.js
@@ -4,61 +4,43 @@ import styled from "styled-components"
 import IconIdeaUp from "../components/icon-idea-up"
 
 const Banner = styled.div`
-  display: grid;
-  grid-template-columns: min-content auto;
-  width: 100%;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: flex-start;
+  align-content: center;
   border-bottom-style: solid;
   border-bottom-width: thin;
   border-bottom-color: rgb(160, 160, 160);
+  padding-bottom: 5px;
 `
 
-const SiteIcon = styled.div`
-  display: table-cell;
-  padding: 5px;
-`
-
-const Hulk = styled.div`
-  display: grid;
-  grid-template-columns: auto;
-  width: 100%;
+const StyledLink = styled(Link)`
+  order: 1;
 `
 
 const BigText = styled.div`
+  order: 2;
   color: coral;
   font-size: 30px;
-  padding-left: 5px;
-  padding-right: 20px;
-  align-self: center;
-  padding-bottom: 8px;
-`
-
-const SmallText = styled.div`
-  color: rgb(176, 176, 176);
-  font-family: Solway, sans-serif;
-  font-size: 22px;
-  align-self: center;
-  padding-bottom: 2px;
+  padding-left: 8px;
 `
 
 const LayoutBannerCompact = () => {
   let siteIconSize = 40
   return (
     <Banner>
-      <SiteIcon>
-        <Link to="/">
-          <IconIdeaUp
-            width={siteIconSize}
-            height={siteIconSize}
-            labelColor="coral"
-            borderColor="coral"
-          />
-        </Link>
-      </SiteIcon>
-      <Hulk>
-        <BigText>
-          <Link to="/">IdeaUp</Link>
-        </BigText>
-      </Hulk>
+      <StyledLink to="/">
+        <IconIdeaUp
+          width={siteIconSize}
+          height={siteIconSize}
+          labelColor="coral"
+          borderColor="coral"
+        />
+      </StyledLink>
+      <BigText>
+        <Link to="/">IdeaUp</Link>
+      </BigText>
     </Banner>
   )
 }
