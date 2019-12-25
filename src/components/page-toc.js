7fba8a683dd8cb548e6b0ea99bb676653e3af291
Rich Seiffert <richard.seiffert@icloud.com>
2019-12-24 18:32:03 -0700

    Removed intersection method of scroll detection; everything is done by an onscroll handler on body now

--- src/components/page-toc.js
+++ src/components/page-toc.js
@@ -13,14 +13,8 @@ const StyledDiv = styled.div`
   }
 `
 
-var observer = null
 var items = []
 var currentItemIdx = -1
-
-var items2 = []
-var currentItemIdx2 = -1
-
-const useIntersectionMethod = false
 
 function removeHighlight(element) {
   if (element) {
@@ -37,85 +31,9 @@ function addHighlight(element) {
 function dumpItems() {
   console.log("start items")
   items.forEach(function(item, idx) {
-    console.log(
-      idx +
-        ": " +
-        (item.isVisible ? "vis" : "novis") +
-        (item.wasAbove ? " above" : " noabove")
-    )
+    console.log(idx + ": " + (item.isAbove ? " above" : " noabove"))
   })
   console.log("end items")
-}
-
-function dumpItems2() {
-  console.log("start items2")
-  items2.forEach(function(item, idx) {
-    console.log(
-      idx +
-        ": " +
-        (item.isVisible ? "vis" : "novis") +
-        (item.isAbove ? " above" : " noabove")
-    )
-  })
-  console.log("end items2")
-}
-
-function intersectionCallback(entries, observer) {
-  if (useIntersectionMethod) {
-    entries.forEach(entry => {
-      items.forEach(function(item, index) {
-        if (item.target === entry.target) {
-          var wasAbove = false
-          if (entry.boundingClientRect && entry.rootBounds) {
-            wasAbove = entry.boundingClientRect.y < entry.rootBounds.y
-          }
-          if (entry.isIntersecting) {
-            item.isVisible = true
-            item.wasAbove = wasAbove
-
-            // If this item is visible, all items
-            // before it are above
-            var i
-            for (i = 0; i < index; i++) {
-              items[i].wasAbove = true
-            }
-          } else {
-            item.isVisible = false
-            item.wasAbove = wasAbove
-          }
-        }
-      })
-    })
-
-    var lastNoVisAboveIdx = -1
-    var hadVisible = false
-    for (const [idx, item] of items.entries()) {
-      if (item.isVisible) {
-        hadVisible = true
-        if (idx !== currentItemIdx) {
-          if (currentItemIdx !== -1) {
-            removeHighlight(
-              document.querySelector(items[currentItemIdx].selector)
-            )
-          }
-          addHighlight(document.querySelector(item.selector))
-          currentItemIdx = idx
-        }
-        break
-      } else {
-        if (item.wasAbove) {
-          lastNoVisAboveIdx = idx
-        }
-      }
-    }
-    if (!hadVisible && lastNoVisAboveIdx !== -1) {
-      if (currentItemIdx !== -1) {
-        removeHighlight(document.querySelector(items[currentItemIdx].selector))
-      }
-      addHighlight(document.querySelector(items[lastNoVisAboveIdx].selector))
-      currentItemIdx = lastNoVisAboveIdx
-    }
-  }
 }
 
 function getViewport() {
@@ -152,12 +70,8 @@ function onBodyScroll() {
   const magicLine = vwDim.height * 0.2
   var changed = false
   var lastAboveIdx = -1
-  items2.forEach(function(item, idx) {
+  items.forEach(function(item, idx) {
     var letsGetRect = item.target.getBoundingClientRect()
-
-    // Is any part of the item within the viewport?
-    const isVisible =
-      letsGetRect.top > 0 - letsGetRect.height && letsGetRect.top < vwDim.height
 
     // Is an part of the item above the magic line?
     const isAbove = letsGetRect.top < magicLine
@@ -165,30 +79,20 @@ function onBodyScroll() {
       lastAboveIdx = idx
     }
 
-    if (isVisible !== item.isVisible || isAbove !== item.isAbove) {
+    if (isAbove !== item.isAbove) {
       changed = true
-      item.isVisible = isVisible
       item.isAbove = isAbove
     }
   })
 
-  if (changed || lastAboveIdx !== currentItemIdx2) {
-    if (!useIntersectionMethod) {
-      if (currentItemIdx2 !== lastAboveIdx) {
-        if (currentItemIdx2 !== -1) {
-          removeHighlight(
-            document.querySelector(items2[currentItemIdx2].selector)
-          )
-        }
-        console.log("Adding highlight to " + lastAboveIdx)
-        addHighlight(document.querySelector(items2[lastAboveIdx].selector))
+  if (changed || lastAboveIdx !== currentItemIdx) {
+    if (currentItemIdx !== lastAboveIdx) {
+      if (currentItemIdx !== -1) {
+        removeHighlight(document.querySelector(items[currentItemIdx].selector))
       }
+      addHighlight(document.querySelector(items[lastAboveIdx].selector))
+      currentItemIdx = lastAboveIdx
     }
-    currentItemIdx2 = lastAboveIdx
-    console.log("items2 changed")
-    dumpItems()
-    dumpItems2()
-    console.log("currentItemIdx2: " + currentItemIdx2)
   }
 }
 
@@ -199,29 +103,10 @@ class PageTOC extends React.Component {
   }
 
   componentDidMount() {
-    var intersectionOptions = {
-      root: null, // use the viewport
-      rootMargin: "0px",
-      threshold: [0, 0.01, 0.99, 1],
-    }
-
-    observer = new IntersectionObserver(
-      intersectionCallback,
-      intersectionOptions
-    )
-
     var target = document.querySelector("#post-header")
     if (target) {
-      observer.observe(target)
       items.push({
         target: target,
-        isVisible: false,
-        wasAbove: false,
-        selector: "#toc-" + target.id,
-      })
-      items2.push({
-        target: target,
-        isVisible: false,
         isAbove: false,
         selector: "#toc-" + target.id,
       })
@@ -236,16 +121,8 @@ class PageTOC extends React.Component {
 
     for (i = 0; i < anchors.length; i++) {
       if (anchors[i].parentElement.tagName.startsWith("H")) {
-        observer.observe(anchors[i].parentElement)
         items.push({
           target: anchors[i].parentElement,
-          isVisible: false,
-          wasAbove: false,
-          selector: "#toc-" + anchors[i].parentElement.id,
-        })
-        items2.push({
-          target: anchors[i].parentElement,
-          isVisible: false,
           isAbove: false,
           selector: "#toc-" + anchors[i].parentElement.id,
         })
@@ -290,11 +167,8 @@ class PageTOC extends React.Component {
   }
 
   componentWillUnmount() {
-    var item = null
-    while ((item = items.pop())) {
-      observer.unobserve(item.target)
-    }
-    observer = null
+    document.body.onscroll = null
+    window.onresize = null
     currentItemIdx = -1
   }
 
