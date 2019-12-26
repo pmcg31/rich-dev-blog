aae2cf9a1b0d9cdace6cee12fce6d8617b12bf44
Rich Seiffert <richard.seiffert@icloud.com>
2019-12-25 17:47:03 -0700

    One more slight tweak for TOC cutoff

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -50,7 +50,7 @@ const PostContent = styled.section`
   grid-template-columns: minmax(0, 1fr) 3fr;
   grid-column-gap: 1em;
   align-items: start;
-  @media (max-width: 40em), (max-height: 60em) {
+  @media (max-width: 40em), (max-height: 50em) {
     grid-template-columns: minmax(0, 1fr);
     #nav-wrapper {
       display: none;
